265a4d2760f9062a616605ca39768c884384c0e6
Conor Doyle <doyle7@hotmail.com>
2017-04-09 17:48:11 +0100

    tidied up js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,47 @@
-var Cat = function() {
-  this.name = ko.observable('Tabby');
+var initialCats = [
+  {
+    clickCount : 0,
+    name : 'Tabby',
+    imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+    imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+    nickname: 'Casper'
+  },
+  {
+    clickCount : 0,
+    name : 'Tiger',
+    imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+    imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+    nickname: 'Felix'
+  },
+  {
+    clickCount : 0,
+    name : 'Scaredy',
+    imgSrc : 'img/22252709_010df3379e_z.jpg',
+    imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
+    nickname: 'Sammy'
+  },
+  {
+    clickCount : 0,
+    name : 'Shadow',
+    imgSrc : 'img/1413379559_412a540d29_z.jpg',
+    imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+    nickname: 'Thomas'
+  },
+  {
+    clickCount : 0,
+    name : 'Sleepy',
+    imgSrc : 'img/9648464288_2516b35537_z.jpg',
+    imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
+    nickname: 'Paula'
+  }
+];
+
+var Cat = function(data) {
+  this.name = ko.observable(data.name);
   this.level = ko.observable();
-  this.nicknames = ko.observableArray([{name: 'Thomas'}, {name:'Conor'}, {name:'Linda'}, {name:'Snufflekins'}, {name:'Paris'}]);
-  this.clickCount = ko.observable(0);
-  this.imgSrc = ko.observable('./img/22252709_010df3379e_z.jpg');
+  this.nickname = ko.observable(data.nickname);
+  this.clickCount = ko.observable(data.clickCount);
+  this.imgSrc = ko.observable(data.imgSrc);
 
   this.increaseLevel = ko.computed(function() {
     if (this.clickCount() < 5) {
@@ -16,16 +54,28 @@ var Cat = function() {
       return this.level('Adult');
     } else if (this.clickCount() <  25) {
       return this.level('Wise Elder');
-
     }
   }, this);
 };
 
 var viewModel = function() {
+  var self = this;
 
-  this.currentCat = ko.observable(new Cat());
+  this.catList = ko.observableArray([]);
+
+  initialCats.forEach(function(catItem) {
+    this.catList.push(new Cat(catItem));
+  }, this);
+
+  this.currentCat = ko.observable(this.catList()[0]);
+
+  this.selectCat = function(cat) {
+    self.currentCat(cat);
+  };
 
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() + 1);
   };
 };
+
+ko.applyBindings(new viewModel());
