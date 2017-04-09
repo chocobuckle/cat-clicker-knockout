9c1024919f9317335aa68764f066260725458364
Conor Doyle <doyle7@hotmail.com>
2017-04-09 18:33:49 +0100

    fix nicknames <ul/>

--- js/app.js
+++ js/app.js
@@ -4,42 +4,42 @@ var initialCats = [
     name : 'Tabby',
     imgSrc : 'img/434164568_fea0ad4013_z.jpg',
     imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
-    nickname: 'Casper'
+    nicknames: ['Casper', 'Molly', 'Jules']
   },
   {
     clickCount : 0,
     name : 'Tiger',
     imgSrc : 'img/4154543904_6e2428c421_z.jpg',
     imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
-    nickname: 'Felix'
+    nicknames: ['Felix', 'Simon', 'Harry']
   },
   {
     clickCount : 0,
     name : 'Scaredy',
     imgSrc : 'img/22252709_010df3379e_z.jpg',
     imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
-    nickname: 'Sammy'
+    nicknames: ['Sammy', 'Meg', 'John']
   },
   {
     clickCount : 0,
     name : 'Shadow',
     imgSrc : 'img/1413379559_412a540d29_z.jpg',
     imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
-    nickname: 'Thomas'
+    nicknames: ['Thomas', 'Peter', 'Willy']
   },
   {
     clickCount : 0,
     name : 'Sleepy',
     imgSrc : 'img/9648464288_2516b35537_z.jpg',
     imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
-    nickname: 'Paula'
+    nicknames: ['Paula', 'Sarah', 'Louise']
   }
 ];
 
 var Cat = function(data) {
   this.name = ko.observable(data.name);
   this.level = ko.observable();
-  this.nickname = ko.observable(data.nickname);
+  this.nicknames = ko.observableArray(data.nicknames);
   this.clickCount = ko.observable(data.clickCount);
   this.imgSrc = ko.observable(data.imgSrc);
 
@@ -61,7 +61,7 @@ var Cat = function(data) {
 var viewModel = function() {
   var self = this;
 
-  this.catList = ko.observableArray([]);
+  this.catList = ko.observableArray();
 
   initialCats.forEach(function(catItem) {
     this.catList.push(new Cat(catItem));
